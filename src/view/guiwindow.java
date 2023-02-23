52f8bdeeab03d63f3665e2a8c934f970e380f58c
Avi <67212741+avi53@users.noreply.github.com>
2023-02-22 23:58:46 -0800

    Added center and south regions to the panel.

--- src/view/guiwindow.java
+++ src/view/guiwindow.java
@@ -1,17 +1,43 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class guiwindow extends JPanel {
     JFrame frame = new JFrame();
 
     public guiwindow() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,600);
+        frame.setSize(500, 600);
         frame.setVisible(true);
         frame.setLayout(null);
-        frame.setResizable(false);
+        frame.setResizable(true);
+
+
+        frame.setLayout(new BorderLayout());
+
+        JPanel centerPanel = new JPanel();
+
+        JPanel colorPanel = new JPanel();
+
+        colorPanel.setBackground(Color.GRAY);
+
+        colorPanel.setPreferredSize(new Dimension(300, 300));
+
+        centerPanel.add(colorPanel);
+
+        frame.add(centerPanel, BorderLayout.CENTER);
+
+        JPanel eastPanel = new JPanel();
+
+        JPanel colorPanel2 = new JPanel();
+
+        colorPanel2.setBackground(Color.BLACK);
+
+        colorPanel2.setPreferredSize(new Dimension(100, 500));
+
+        eastPanel.add(colorPanel2);
+
+        frame.add(eastPanel, BorderLayout.EAST);
     }
-
-
 }
