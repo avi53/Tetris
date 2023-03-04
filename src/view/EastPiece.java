f27b33392c16fe23beeec7ab940bf777efba33f9
d4hy <davidhoang253@gmail.com>
2023-03-03 17:35:50 -0800

    Implemented entire east panel to be the next piece.

--- src/view/EastPiece.java
+++ src/view/EastPiece.java
@@ -9,17 +9,16 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import javax.swing.JPanel;
 
-public class EastPiece extends JPanel {
+public class EastPiece extends JPanel implements PropertyChangeListener {
+
+
     /** East piece width.*/
     private static final int EAST_WIDTH = 200;
 
     /** East piece height.*/
     private static final int EAST_HEIGHT = 500;
 
-    /** Blue square width.*/
-    private static final int BLUE_WIDTH = 200;
-    /** Blue square height.*/
-    private static final int BLUE_HEIGHT = 200;
+
     /** East piece JPanel.*/
     private final JPanel myEastPanel = new JPanel();
     /**
@@ -33,46 +32,19 @@ public class EastPiece extends JPanel {
     public EastPiece() {
         super();
         createEastPiece();
-        createNextPiecePanel();
     }
 
     /**
      * Create the east piece width, height, and set the color.
      */
     private void createEastPiece() {
-        myEastPanel.setBackground(Color.BLACK);
+        myEastPanel.setBackground(Color.BLUE);
         myEastPanel.setPreferredSize(new Dimension(EAST_WIDTH, EAST_HEIGHT));
         add(myEastPanel, BorderLayout.EAST);
     }
 
-    private void createNextPiecePanel() {
-        final NextPiecePanel nextPiecePanel = new NextPiecePanel();
-        myEastPanel.add(nextPiecePanel);
-        tetrisBoard.addPropertyChangeListener(PropertyChangeEnabledBoardControls.PROPERTY_STEP, nextPiecePanel);
-    }
 
-    /**
-     * Create the panel which displays the next piece.
-     */
-    private static final class NextPiecePanel extends JPanel implements PropertyChangeListener {
-        private final JPanel myNextPiecePanel = new JPanel();
 
-        private NextPiecePanel() {
-            super();
-            createAndShowPanel();
-
-        }
-
-        public void paintComponent(final Graphics theGraphics) {
-
-        }
-
-        private void createAndShowPanel() {
-            myNextPiecePanel.setBackground(Color.BLUE);
-            myNextPiecePanel.setPreferredSize(new Dimension(BLUE_WIDTH, BLUE_HEIGHT));
-            add(myNextPiecePanel);
-
-        }
 
         @Override
         public void propertyChange(PropertyChangeEvent theEvent) {
@@ -81,4 +53,4 @@ public class EastPiece extends JPanel {
             }
         }
     }
-}
+
