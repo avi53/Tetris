aaa4753642d69d6fa19fe767a678733e5b2fddfd
Avi <67212741+avi53@users.noreply.github.com>
2023-03-08 23:22:03 -0800

    -added for loop to loop through the frozen blocks

--- src/view/CenterPanel.java
+++ src/view/CenterPanel.java
@@ -10,9 +10,9 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.LinkedList;
 import javax.swing.JPanel;
-
 import model.Block;
 import model.Board;
+
 public class CenterPanel extends JPanel implements PropertyChangeListener {
     /**
      * center width.
@@ -29,22 +29,34 @@ public class CenterPanel extends JPanel implements PropertyChangeListener {
     private static final int PIECE_SIZE = 50;
 
     /** temp height setting. */
-    private static final int HEIGHT = 500;
+    private static final int HEIGHT = 20;
 
     /** temp width setting. */
-    private static final int WIDTH = 300;
+    private static final int WIDTH = 10;
 
+    /** shape. */
     private final Rectangle2D myShape;
 
 
     /** The background color. */
-    private  LinkedList<Block[]> myFrozenBlocks;
-
+    private  LinkedList<Block []> myFrozenBlocks;
 
     public CenterPanel() {
         super();
         createCenterPiece();
-        myShape = new Rectangle2D.Double(0,0,50,50);
+        myShape = new Rectangle2D.Double(0 , 0, 50, 50);
+
+        myFrozenBlocks = new LinkedList<>();
+
+        for (int i = 0; i < HEIGHT; i++) {
+            final Block[] row = new Block[WIDTH];
+
+            for (int j = 0; j < WIDTH; j++) {
+                row[j] = Block.EMPTY;
+            }
+
+            myFrozenBlocks.add(row);
+        }
     }
 
     private void createCenterPiece() {
@@ -62,30 +74,45 @@ public class CenterPanel extends JPanel implements PropertyChangeListener {
                 propertyName.equals(Board.PROPERTY_GAME_OVER)
                 ||
                 propertyName.equals(Board.PROPERTY_SEQUENCE_INDEX)) {
+
             //when board state changes everytime the board (center component) gets repainted
             repaint();
-        }else if (propertyName.equals(Board.PROPERTY_FROZEN_BLOCKS_SIZE)) {
+
+        } else if (propertyName.equals(Board.PROPERTY_FROZEN_BLOCKS_SIZE)) {
             myFrozenBlocks = (LinkedList<Block[]>) theEvt.getNewValue();
+
+        } else if ("rotation".equals(theEvt.getPropertyName())) {
+            repaint();
         }
     }
+
+    
 
     @Override
     public void paintComponent(final Graphics theGraphics) {
         super.paintComponent(theGraphics);
         final Graphics2D g2d = (Graphics2D) theGraphics;
 
-        // going through the frozen blocks list.
-       // for(int i=0; i< myFrozenBlocks.size();i++) {
-          // final Block[] row= myFrozenBlocks.get(i);
-           // for (int j=0; j< row.length; j++) {
-             //   row[j]= Block.J;
-           // }
-        //}
-
         // for better graphics display
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
 
+        // going through the frozen blocks list.
+        for (int i = 0; i < myFrozenBlocks.size(); i++) {
+
+            final Block[] row = myFrozenBlocks.get(i);
+
+            for (int j = 0; j < row.length; j++) {
+
+                if (row[j] != Block.EMPTY) {
+                    g2d.setPaint(Color.RED);
+                    g2d.fill(new Rectangle2D.Double(j * PIECE_SIZE,
+                           i * PIECE_SIZE,
+                           PIECE_SIZE,
+                           PIECE_SIZE));
+                }
+            }
+        }
         g2d.setPaint(Color.RED);
 
         for (int row = 0; row < HEIGHT; row++) {
